5a626cb103795c23bb61201ffe552ccacddf2f05
konakov-ds <konakov-ds@yandex.ru>
2022-04-25 15:59:34 +0300

    add func to get info from api

--- bot.py
+++ bot.py
@@ -7,3 +7,14 @@ env.read_env()
 
 
 devman_api_token = env('DEVMAN_TOKEN')
+dewman_api_url = 'https://dvmn.org/api/user_reviews/'
+
+
+def get_user_assessments(url, token):
+    headers = {'Authorization': f'Token {devman_api_token}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.text
+
+
+print(get_user_assessments(dewman_api_url, devman_api_token))
