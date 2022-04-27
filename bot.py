5a22e6cccef77bf73ea30b4b45290bf59f5ffb9f
konakov-ds <konakov-ds@yandex.ru>
2022-04-27 08:17:50 +0300

    remove requests wrapper

--- bot.py
+++ bot.py
@@ -16,15 +16,6 @@ dewman_api_url = 'https://dvmn.org/api/user_reviews/'
 dewman_api_url_long = 'https://dvmn.org/api/long_polling/'
 
 
-def get_user_assessments(url, token, timestamp, timeout=30, params=None):
-    headers = {'Authorization': f'Token {token}'}
-    if timestamp:
-        params = {'timestamp': timestamp}
-    response = requests.get(url, headers=headers, params=params, timeout=timeout)
-    response.raise_for_status()
-    return response.json()
-
-
 def send_message_from_bot(chat_id, response=None):
     bot = telegram.Bot(token=telegram_token)
     if response:
@@ -42,23 +33,27 @@ def send_message_from_bot(chat_id, response=None):
             )
 
 
-def loop(chat_id, last_attempt_timestamp=None):
+def bot_loop(
+        url,
+        token,
+        chat_id,
+        timeout=30,
+        last_attempt_timestamp=None
+):
     while True:
         try:
-            response = get_user_assessments(
-                dewman_api_url_long,
-                devman_api_token,
-                timestamp=last_attempt_timestamp,
-            )
+            headers = {'Authorization': f'Token {token}'}
+            params = {'timestamp': last_attempt_timestamp}
+            response = requests.get(url, headers=headers, params=params, timeout=timeout)
+            response.raise_for_status()
             if response:
+                response = response.json()
                 last_attempt_timestamp = response['last_attempt_timestamp']
                 attempts = response['new_attempts']
                 for attempt in attempts:
                     send_message_from_bot(chat_id, response=attempt)
-            continue
         except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
             sleep(5)
-            loop(chat_id, last_attempt_timestamp)
 
 
 if __name__ == '__main__':
@@ -66,4 +61,8 @@ if __name__ == '__main__':
     parser.add_argument('--chat_id')
     args = parser.parse_args()
 
-    loop(chat_id=args.chat_id)
+    bot_loop(
+        url=dewman_api_url_long,
+        token=devman_api_token,
+        chat_id=args.chat_id,
+    )
